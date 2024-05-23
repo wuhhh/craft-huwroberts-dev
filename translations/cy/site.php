a981bca86548845cfba184d6a9c736d195207ba1
Huw Roberts <huwrroberts@gmail.com>
2024-05-23 17:53:41 +0100

    Add static translations

--- translations/cy/site.php
+++ translations/cy/site.php
@@ -1,4 +1,8 @@
 <?php
 	return [
 		"View details, opens dialog" => "Gweld manylion, yn agor deialog",
+		"Who?" => "Pwy?",
+		"Say hello!" => "Dweud helo!",
+		"home" => "cartref",
+		"Skip to content" => "Neidio i gynnwys",
 	];
