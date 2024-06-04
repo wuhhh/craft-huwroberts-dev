f91f6347d3b29a1f97345c71d080ee720b96e00e
Huw Roberts <huwrroberts@gmail.com>
2024-06-04 13:39:26 +0100

    Add bounce ease to tw conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,6 +44,9 @@ module.exports = {
       minHeight: {
         screenh: "100dvh",
       },
+      transitionTimingFunction: {
+        "bounce": "cubic-bezier(0.175, 0.885, 0.32, 1.275)",
+      },
     },
   },
 };
