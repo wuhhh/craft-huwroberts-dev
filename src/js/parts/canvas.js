1c83a813c3a6818e5dafd1737bc087f6a1dfd447
Huw Roberts <huwrroberts@gmail.com>
2024-06-08 15:39:35 +0100

    Achieved more consistent canvas+scene scaling

--- src/js/parts/canvas.js
+++ src/js/parts/canvas.js
@@ -1,7 +1,14 @@
-import { Camera, Mesh, Plane, Program, Renderer, Texture, Transform } from "ogl";
-import { loadTexture } from './loaders';
+import {
+  Camera,
+  Mesh,
+  Plane,
+  Program,
+  Renderer,
+  Texture,
+  Transform,
+} from "ogl";
+import { loadTexture } from "./loaders";
 import { vertex, fragment } from "./shaders";
-
 
 export default ("canvas",
 () => ({
@@ -9,7 +16,10 @@ export default ("canvas",
   mesh: null,
   program: null,
   renderer: null,
+  retainScaleOnResize: false,
   scene: null,
+  sunScale: 1,
+  tanFOV: null,
 
   async init() {
     this.aspectRatio = window.innerWidth / window.innerHeight;
@@ -32,9 +42,14 @@ export default ("canvas",
     this.camera.position.set(0, 0, 2.5);
     this.camera.lookAt([0, 0, 0]);
 
+    this.tanFOV = Math.tan(((Math.PI / 180) * this.camera.fov) / 2);
+    this.windowHeight = window.innerHeight;
+
+    console.log("this.camera", this.camera);
+
     // plane geometry
     // const geometry = new Plane(this.gl, { width: .4, height: .4 });
-    const geometry = new Plane(this.gl, { width: .75, height: .75 });
+    const geometry = new Plane(this.gl, { width: this.sunScale, height: this.sunScale });
 
     // shader
     this.program = new Program(this.gl, {
@@ -48,17 +63,20 @@ export default ("canvas",
     });
 
     // load texture
-    const texture = await loadTexture('/dist/images/texture--sun-alt12@2x.png', this.program.gl);
+    const texture = await loadTexture(
+      "/dist/images/texture--sun-alt12@2x.png",
+      this.program.gl
+    );
     this.program.uniforms.uTexture.value = texture;
 
     this.scene = new Transform();
 
     // mesh
     this.mesh = new Mesh(this.gl, { geometry, program: this.program });
-    this.mesh.position.set(.8, .35, 0);
+    this.mesh.position.set(0.8, 0.35, 0);
     this.mesh.setParent(this.scene);
 
-    window.addEventListener('resize', () => this.resize(), false)
+    window.addEventListener("resize", () => this.resize(), false);
     this.resize();
 
     // start draw loop
@@ -77,12 +95,32 @@ export default ("canvas",
     this.renderer.render({ scene: this.scene, camera: this.camera });
   },
 
+  setSunScale() {
+    const scale =
+      (Math.min(this.gl.canvas.width, 1568) / this.gl.canvas.height) * this.sunScale;
+    this.mesh.scale.x = scale;
+    this.mesh.scale.y = scale;
+  },
+
   /**
    * Resize canvas
    */
   resize() {
-    console.log('resize');
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.camera.perspective({ aspect: this.gl.canvas.width / this.gl.canvas.height });
+    if (!this.retainScaleOnResize) {
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.camera.perspective({
+        aspect: this.gl.canvas.width / this.gl.canvas.height,
+      });
+      this.setSunScale();
+    } else {
+      this.camera.perspective({
+        aspect: window.innerWidth / window.innerHeight,
+        fov:
+          (360 / Math.PI) *
+          Math.atan(this.tanFOV * (window.innerHeight / this.windowHeight)),
+      });
+      this.camera.lookAt(this.scene.position);
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
   },
 }));
