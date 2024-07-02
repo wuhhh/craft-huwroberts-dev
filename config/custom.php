ef67385f02a510969d3687b65fc106c05a9b16b2
Huw Roberts <huwrroberts@gmail.com>
2024-07-02 11:50:20 +0100

    Replace seomatic with seomate

---
+++ config/custom.php
@@ -0,0 +1,7 @@
+<?php
+
+use craft\helpers\App;
+
+return [
+	'environment' => (bool) App::env('ENVIRONMENT'),
+];
---
+++ config/seomate.php
@@ -0,0 +1,41 @@
+<?php
+
+use craft\helpers\App;
+
+return [
+	'cacheEnabled' => (bool) !App::env('DEV_MODE'),
+	'siteName' => '{{ seo.siteName }}',
+	'sitenamePosition' => 'before',
+
+    'defaultMeta' => [
+        'title' => ['seo.seoTitle'],
+        'description' => ['seo.seoDescription'],
+		'keywords' => ['seo.seoKeywords'],
+        'image' => ['seo.seoImage'],
+		'og:title' => ['seo.seoTitle'],
+		'og:image' => ['seo.seoImage'],
+		'twitter:image' => ['seo.seoTwitterImage'],
+    ],
+
+	'additionalMeta' => [
+        'og:type' => 'website',
+        'twitter:card' => 'summary_large_image',
+
+        'twitter:site' => '@{{ seo.twitterHandle }}',
+        'twitter:author' => '@{{ seo.twitterHandle }}',
+        'twitter:creator' => '@{{ seo.twitterHandle }}',
+
+        'og:see_also' => function ($context) {
+            $seeAlsoLinks = [];
+            $seeAlso = $context['seo']->seeAlso;
+
+            if (!empty($seeAlso)) {
+                foreach ($seeAlso as $also) {
+                    $seeAlsoLinks[] = $also['url'];
+                }
+            }
+
+            return $seeAlsoLinks;
+        },
+    ],
+];
