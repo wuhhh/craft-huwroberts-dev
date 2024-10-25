74abfbadbe92f2d1644a2680f5b810045af51d38
Huw Roberts <huwrroberts@gmail.com>
2024-10-25 22:17:45 +0100

    Return of the canvas

--- src/js/app.js
+++ src/js/app.js
@@ -15,7 +15,7 @@ import intersect from '@alpinejs/intersect';
 import swipePlugin from "alpinejs-swipe";
 import ui from '@alpinejs/ui';
 
-// import canvas from './parts/canvas';
+import canvas from './parts/canvas';
 import core from './parts/core';
 // import emoji from './parts/emoji';
 import follower from './parts/follower';
@@ -26,7 +26,7 @@ Alpine.plugin(intersect);
 Alpine.plugin(swipePlugin);
 Alpine.plugin(ui);
 
-// Alpine.data('canvas', canvas);
+Alpine.data('canvas', canvas);
 Alpine.data('core', core);
 Alpine.data('follower', follower);
 // Alpine.data('emoji', emoji);
