55d7052798287396cc4f74af6bf1250c8a64bb5c
Huw Roberts <huw@huwroberts.dev>
2025-01-09 15:29:42 +0000

    Use a method to open slideover

--- src/js/stores/global.js
+++ src/js/stores/global.js
@@ -1,11 +1,20 @@
 export default () => ({
   mouseCoords: [0, 0],
   slideoverOpen: false,
-  slideoverTemplate: '',
+  slideoverTemplate: "",
 
   init() {
-    window.addEventListener('mousemove', (e) => {
-      this.mouseCoords = [e.clientX, e.clientY]
-    }, { passive: true });
-  }
+    window.addEventListener(
+      "mousemove",
+      (e) => {
+        this.mouseCoords = [e.clientX, e.clientY];
+      },
+      { passive: true }
+    );
+  },
+
+  openSlideover() {
+    this.slideoverOpen = true;
+    window.location.hash = "slideover";
+  },
 });
