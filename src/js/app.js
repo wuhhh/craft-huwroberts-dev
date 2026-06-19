122e7a89aea319db930ac20b14dcab649d6e53a1
Huw Roberts <huw@huwroberts.dev>
2026-06-19 21:54:27 +0100

    chore: webgpu elements poc

--- src/js/app.js
+++ src/js/app.js
@@ -2,41 +2,41 @@
 import "@js/parts/css";
 
 /* Non-alpine JS */
-
+import "@js/parts/threeElements";
 
 /**
  * Alpine JS
-*/
-import Alpine from 'alpinejs';
-import focus from '@alpinejs/focus';
-import intersect from '@alpinejs/intersect';
+ */
+import Alpine from "alpinejs";
+import focus from "@alpinejs/focus";
+import intersect from "@alpinejs/intersect";
 import swipePlugin from "alpinejs-swipe";
-import ui from '@alpinejs/ui';
+import ui from "@alpinejs/ui";
 
-import canvas from './parts/canvas';
-import core from './parts/core';
-import follower from './parts/follower';
-import mediaControls from './parts/mediaControls';
-import slideover from './parts/slideover';
+import canvas from "./parts/canvas";
+import core from "./parts/core";
+import follower from "./parts/follower";
+import mediaControls from "./parts/mediaControls";
+import slideover from "./parts/slideover";
 
-import about from './stores/about';
-import global from './stores/global';
-import work from './stores/work';
+import about from "./stores/about";
+import global from "./stores/global";
+import work from "./stores/work";
 
 Alpine.plugin(focus);
 Alpine.plugin(intersect);
 Alpine.plugin(swipePlugin);
 Alpine.plugin(ui);
 
-Alpine.data('canvas', canvas);
-Alpine.data('core', core);
-Alpine.data('follower', follower);
-Alpine.data('mediaControls', mediaControls);
-Alpine.data('slideover', slideover);
+Alpine.data("canvas", canvas);
+Alpine.data("core", core);
+Alpine.data("follower", follower);
+Alpine.data("mediaControls", mediaControls);
+Alpine.data("slideover", slideover);
 
-Alpine.store('about', about());
-Alpine.store('global', global());
-Alpine.store('work', work());
+Alpine.store("about", about());
+Alpine.store("global", global());
+Alpine.store("work", work());
 
 Alpine.start();
 
