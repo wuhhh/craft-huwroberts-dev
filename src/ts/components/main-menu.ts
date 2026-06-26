43b7a2ba9f4f848c59d757c617acc1eaba2ac424
Huw Roberts <huw@huwroberts.dev>
2026-06-26 15:49:51 +0100

    feat: main menu responsiveness

--- src/ts/components/main-menu.ts
+++ src/ts/components/main-menu.ts
@@ -7,24 +7,26 @@ export class MainMenu extends LitElement {
   open = false;
 
   static styles?: CSSResultGroup | undefined = css`
-    :host {
+    #menu {
       position: relative;
     }
 
     button {
-      width: 2.75rem;
-      height: 2.75rem;
-      display: block;
-      margin-inline: auto;
-      position: relative;
-
       appearance: none;
       background: none;
       border: 0;
       border-radius: 0;
     }
 
-    button > span {
+    button.toggle {
+      width: 2.75rem;
+      height: 2.75rem;
+      display: block;
+      margin-inline: auto;
+      position: relative;
+    }
+
+    button.toggle > span {
       display: flex;
       flex-direction: column;
       justify-content: center;
@@ -32,7 +34,7 @@ export class MainMenu extends LitElement {
       gap: 0.25rem;
     }
 
-    button > span > span {
+    button.toggle > span > span {
       width: 1.375rem;
       height: 0.125rem;
       background: var(--color-seabed-indigo);
@@ -40,53 +42,128 @@ export class MainMenu extends LitElement {
       transform-origin: left;
     }
 
-    button:hover > span > span:first-child {
+    button.toggle:hover > span > span:first-child {
       transform: scaleX(0.7);
     }
 
-    button:hover > span > span:nth-child(2) {
+    button.toggle:hover > span > span:nth-child(2) {
       transform: scaleX(0.5);
     }
 
-    button:hover > span > span:nth-child(3) {
+    button.toggle:hover > span > span:nth-child(3) {
       transform: scaleX(0.9);
     }
 
+    button.close {
+      display: grid;
+      place-content: center;
+      position: fixed;
+      top: 0.75rem;
+      right: 0.75rem;
+      width: 2.75rem;
+      height: 2.75rem;
+      border-radius: 9999px;
+      background-color: var(--color-stone);
+      color: var(--color-seabed-indigo);
+      opacity: 0;
+      pointer-events: none;
+      transition: opacity 325ms ease;
+      transition-delay: 125ms;
+    }
+
+    #menu.open button.close {
+      pointer-events: auto;
+    }
+
     nav {
-      position: absolute;
-      left: 4rem;
-      top: 0.4375rem;
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      width: clamp(260px, 400px, 66vw);
+      background-color: var(--color-seabed-indigo);
       display: flex;
       flex-direction: column;
       align-items: flex-start;
-      row-gap: 0.125rem;
+      row-gap: 1rem;
+      padding: 0.75rem 1.5rem;
+      opacity: 0;
+      transform: translateX(2rem);
+      transition:
+        opacity 325ms ease,
+        transform 500ms ease;
+    }
+
+    nav a:first-child {
+      margin-top: 4.5rem;
+    }
+
+    #menu.open nav {
+      opacity: 1;
+      transform: translateX(0);
+    }
+
+    #menu.open button.close {
+      opacity: 1;
+      transition-delay: 0;
     }
 
     a {
       color: var(--color-stone);
-      display: inline-block;
       font-family: var(--font-mono);
-      font-size: var(--text-xs);
-      line-height: var(--text-xs--line-height);
+      font-size: 1.25rem;
       text-transform: uppercase;
-      background-color: var(--color-seabed-indigo);
-      padding: 0.375rem 1.5rem !important;
       clip-path: inset(0px 100% 0px 0px);
       transition: clip-path 325ms cubic-bezier(0.785, 0.135, 0.15, 0.86);
       will-change: clip-path;
       text-decoration: none;
     }
 
-    a:hover {
-      background-color: var(--color-coral-red);
-    }
-
-    a:focus-visible {
-      box-shadow: inset 0 0 0 2px var(--color-coral-red);
-    }
-
-    nav.open a {
+    #menu.open a {
       clip-path: inset(0px 0% 0px 0px);
+    }
+
+    @media (min-width: 640px) {
+      #menu a:first-child {
+        margin-top: 0;
+      }
+
+      button.close {
+        opacity: 0 !important;
+        pointer-events: none !important;
+      }
+
+      nav {
+        position: absolute;
+        left: 4rem;
+        top: 0.4375rem;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        row-gap: 0.125rem;
+
+        padding: 0;
+        background-color: transparent;
+        opacity: 1;
+        transform: none;
+        transition: none;
+      }
+
+      a {
+        display: inline-block;
+        font-size: var(--text-xs);
+        line-height: var(--text-xs--line-height);
+        background-color: var(--color-seabed-indigo);
+        padding: 0.375rem 1.5rem !important;
+      }
+
+      a:hover {
+        background-color: var(--color-coral-red);
+      }
+
+      a:focus-visible {
+        box-shadow: inset 0 0 0 2px var(--color-coral-red);
+      }
     }
   `;
 
@@ -103,7 +180,6 @@ export class MainMenu extends LitElement {
   }
 
   handleClickAway = (event: MouseEvent) => {
-    console.log(event);
     if (event.target && !this.contains(event.target as Node)) this.closeMenu();
   };
 
@@ -114,27 +190,43 @@ export class MainMenu extends LitElement {
 
   render() {
     return html`
-      <button
-        @click=${this.toggleMenu}
-        aria-label="${this.open ? `Hide` : `Show`} menu"
-      >
-        <span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-      </button>
-      <nav
-        ?inert=${!this.open}
-        class="${this.open ? `open` : ``}"
-        aria-label="Main"
-      >
-        <a href="/">Home</a>
-        <a href="/work" style="transition-delay: 30ms">Portfolio</a>
-        <a href="/about" style="transition-delay: 60ms">About</a>
-        <a href="/journal" style="transition-delay: 90ms">Journal</a>
-        <a href="/contact" style="transition-delay: 120ms">Contact</a>
-      </nav>
+      <div id="menu" class="${this.open ? `open` : ``}">
+        <!-- desktop menu toggle -->
+        <button
+          class="toggle"
+          @click=${this.toggleMenu}
+          aria-label="${this.open ? `Hide` : `Show`} menu"
+        >
+          <span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </button>
+        <!-- nav -->
+        <nav ?inert=${!this.open} aria-label="Main">
+          <a href="/">Home</a>
+          <a href="/work" style="transition-delay: 30ms">Portfolio</a>
+          <a href="/about" style="transition-delay: 60ms">About</a>
+          <a href="/journal" style="transition-delay: 90ms">Journal</a>
+          <a href="/contact" style="transition-delay: 120ms">Contact</a>
+        </nav>
+        <!-- mobile menu close -->
+        <button class="close" @click=${this.closeMenu}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="17"
+            height="17"
+            fill="none"
+            viewBox="0 0 17 17"
+          >
+            <path
+              fill="currentColor"
+              d="M16.97 1.414 9.9 8.485l7.07 7.072-1.413 1.414-7.072-7.072-7.07 7.072L0 15.557l7.071-7.072L0 1.415 1.414 0l7.071 7.071L15.557 0z"
+            />
+          </svg>
+        </button>
+      </div>
     `;
   }
 }
